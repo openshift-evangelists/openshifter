eb2f05bb5e40134e7aaf4e77af30734ba5c6328f
Marek Jelen <marek@jelen.biz>
2017-03-27 10:07:06 +0200

    CLI updates

--- terraform.go
+++ terraform.go
@@ -1,26 +1,62 @@
 package main
 
 import (
-	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"io/ioutil"
 	"os"
 	"os/exec"
-	"strconv"
-	"strings"
+	"text/template"
+	"fmt"
+	"io"
+	"bytes"
 )
+
+// Process deployment and prepare Terraform environment to run the tool against
+func RenderTemplate(deployment Deployment) {
+	log.Info("Generating Terraform data")
+
+	err := os.MkdirAll(deployment.Name + ".data", os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := Asset("templates/aws.tf")
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(deployment.Name + ".data/aws.tf")
+	if err != nil {
+		panic(err)
+	}
+
+	io.Copy(file, bytes.NewReader(data))
+
+	data, err = Asset("templates/variables.tf")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, err := template.New("terraform").Parse(string(data))
+	if err != nil {
+		panic(err)
+	}
+
+	file, err = os.Create(deployment.Name + ".data/variables.tf")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(file, deployment)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // Provision executes a Terraform template
 // based on the provided deployment, provisioning a cluster.
-func Provision(dpl Deployment) {
-	err := os.MkdirAll("tmp/", os.ModePerm)
-	if err != nil {
-		log.WithFields(log.Fields{"func": "Provision"}).Error(fmt.Sprintf("Can't create temporary working directory for Terraform %s", err))
-		return
-	}
-	copy("templates/main.tf", "tmp/main.tf")
-	replace("templates/variables.tf", "tmp/variables.tf")
-	err = run("apply", "tmp/")
+func Provision(deployment Deployment) {
+	log.Info("Creating new cluster")
+	err := run("apply", deployment.Name + ".data")
 	if err != nil {
 		log.WithFields(log.Fields{"func": "Provision"}).Error(fmt.Sprintf("Can't run Terraform %s", err))
 	}
@@ -28,33 +64,52 @@ func Provision(dpl Deployment) {
 
 // Deprovision executes a Terraform template
 // based on the provided deployment.
-func Deprovision(dpl Deployment) {
-	err := run("destroy", "-force", "tmp/")
+func Deprovision(deployment Deployment) {
+	log.Info("Destroying cluster")
+	err := run("destroy", "-force",  deployment.Name + ".data")
 	if err != nil {
 		log.WithFields(log.Fields{"func": "Deprovision"}).Error(fmt.Sprintf("Can't run Terraform %s", err))
 	}
 }
 
-func replace(from, to string) error {
-	b, err := ioutil.ReadFile(from)
-	if err != nil {
-		return err
-	}
-	v := strings.Replace(string(b), "AWS_REGION", dpl.Provider.Region, 1)
-	v = strings.Replace(v, "MACHINE_TYPE", dpl.Provider.Machine, 1)
-	v = strings.Replace(v, "NUM_WORKER_NODES", strconv.Itoa(dpl.Provider.Nodes), 1)
-	err = ioutil.WriteFile(to, []byte(v), 0644)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func run(cmd ...string) error {
-	out, err := exec.Command("terraform", cmd...).Output()
+	log.Info("Executing Terraform")
+	command := exec.Command("terraform", cmd...)
+
+	var stdout, stderr bytes.Buffer
+	command.Stdout = &stdout
+	command.Stderr = &stderr
+
+	err := command.Start()
 	if err != nil {
 		return err
 	}
-	log.WithFields(log.Fields{"func": "run"}).Info(fmt.Sprintf("%s", out))
+
+	go func() {
+		for {
+			line, err := stdout.ReadBytes('\n')
+			if err != nil {
+				if err != io.EOF { log.Error("Problem reading stdout ", err) }
+				break
+			}
+			log.WithFields(log.Fields{"source": "Terraform"}).Info(line)
+		}
+	}()
+
+	go func() {
+		for {
+			line, err := stderr.ReadBytes('\n')
+			if err != nil {
+				if err != io.EOF { log.Error("Problem reading stderr ", err) }
+				break
+			}
+			log.WithFields(log.Fields{"source": "Terraform"}).Error(line)
+		}
+	}()
+
+	command.Wait()
+
+	log.Info("Terraform finished")
+
 	return nil
 }
