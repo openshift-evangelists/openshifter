bf88d8bb670be6ae15af6e03ba46e787a202fe1a
Marek Jelen <marek@jelen.biz>
2017-07-21 12:48:07 +0200

    More work on deployment management

--- web/__init__.py
+++ web/__init__.py
@@ -3,72 +3,140 @@ import os.path
 import json
 import asyncio
 import asyncio.subprocess
+import logging
 
 import aiohttp_jinja2
 import jinja2
 
 from aiohttp import web
 
-listeners = {}
-processes = {}
 
-async def read(name, source, stream):
-    while not stream.at_eof():
-        data = await stream.readline()
-        if len(data) > 0:
-            for listener in listeners[name]:
-                if listener.closed:
-                    listeners[name].remove(listener)
+class WebsocketListener:
+    def __init__(self, websocket):
+        self.websocket = websocket
+
+    async def close(self):
+        await self.websocket.close()
+
+    def closed(self):
+        return self.websocket.closed
+
+    async def send(self, data):
+        await self.websocket.send_str(data)
+
+class Deployments:
+
+    def __init__(self):
+        self.listeners = {}
+        self.processes = {}
+        self.logger = logging.getLogger("Deployments")
+
+    async def add_listener(self, name, listener):
+        if name not in self.listeners:
+            self.listeners[name] = []
+        self.logger.info("Registering new listener")
+        self.listeners[name].append(listener)
+        if name in self.processes:
+            await self.send(name, {'type': 'started'})
+
+    def remove_listener(self, name, listener):
+        if name in self.listeners:
+            self.logger.info("Removing listener")
+            self.listeners[name].remove(listener)
+
+    async def create(self, name):
+        if name not in self.processes:
+            self.logger.info("Starting new deployment creation")
+            await self.execute('create', name)
+
+    async def destroy(self, name):
+        if name not in self.processes:
+            self.logger.info("Starting deployment destruction")
+            await self.execute('destroy', name)
+
+    async def execute(self, action, name):
+        cmd = os.path.join(os.environ['ROOT_DIR'], 'entrypoint.sh')
+        cmd += ' ' + action + ' ' + name
+        process = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        self.processes[name] = process
+        asyncio.ensure_future(self._read_stream(name, 'out', process.stdout))
+        asyncio.ensure_future(self._read_stream(name, 'err', process.stderr))
+        await self.send(name, {'type': 'started'})
+        ec = await process.wait()
+        await self.completed(name, ec)
+
+    async def completed(self, name, ec):
+        msg = {'type': 'completed', 'success': True}
+        if ec > 0:
+            msg['success'] = False
+            msg['code'] = ec
+        await self.send(name, msg)
+
+        for listener in self.listeners[name]:
+            await listener.close()
+
+        self.processes.pop(name)
+
+    async def send(self, name, data):
+        if name in self.listeners:
+            for listener in self.listeners[name]:
+                if listener.closed():
+                    self.listeners[name].remove(listener)
                 else:
-                    listener.send_str(json.dumps({'source': source, 'line': data.decode("utf-8")}))
+                    await listener.send(json.dumps(data))
 
-async def manage_deployment(action, name):
-    cmd = os.path.join(os.environ['ROOT_DIR'], 'entrypoint.sh')
-    cmd += ' ' + action + ' ' + name
-    process = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    processes[name] = process
-    asyncio.ensure_future(read(name, 'out', process.stdout))
-    asyncio.ensure_future(read(name, 'err', process.stderr))
-    await process.wait()
-    processes.pop(name)
-    for listener in listeners[name]:
-        await listener.close()
+    async def _read_stream(self, name, source, stream):
+        while not stream.at_eof():
+            data = await stream.readline()
+            if len(data) > 0:
+                await self.send(name, {'type': 'log', 'source': source, 'line': data.decode("utf-8")})
+
+    async def deployments_handler(self, request):
+        response = []
+        for item in os.listdir("."):
+            if os.path.isfile(item) and item.endswith(".yml"):
+                name = item.replace(".yml", "")
+                item = {
+                    'name': name
+                }
+                if name in self.processes:
+                    item['running'] = True
+                response.append(item)
+        return web.Response(text=json.dumps(response), content_type='application/json')
+
+    async def create_deployment_handler(self, request):
+        await self.create(request.match_info['name'])
+        return web.Response(text="{}", content_type='application/json')
+
+    async def destroy_deployment_handler(self, request):
+        await self.destroy(request.match_info['name'])
+        return web.Response(text="{}", content_type='application/json')
+
+
+deployments = Deployments()
 
 async def schema(request):
     with open(os.path.join(os.path.dirname(__file__), '..', 'schema.yml'), 'r') as file:
         return web.Response(text=file.read(), content_type='application/x-yaml')
 
-async def deployments(request):
-    response = []
-    for item in os.listdir("."):
-        if os.path.isfile(item) and item.endswith(".yml"):
-            response.append(item.replace(".yml", ""))
-    return web.Response(text=json.dumps(response), content_type='application/json')
-
-async def new_deployment(request):
-    name = request.match_info['name']
-    if name not in processes:
-        await manage_deployment('create', name)
-    return web.Response(text="{}", content_type='application/json')
-
-async def delete_deployment(request):
-    name = request.match_info['name']
-    if name not in processes:
-        await manage_deployment('destroy', name)
-    return web.Response(text="{}", content_type='application/json')
-
 async def stream(request):
-    name = request.match_info['name']
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-    if name not in listeners:
-        listeners[name] = []
-    listeners[name].append(ws)
-    async for msg in ws:
-        if msg.data == 'close':
-            await ws.close()
-    listeners[name].remove(ws)
-    return ws
+    websocket = web.WebSocketResponse()
+    await websocket.prepare(request)
+    listener = WebsocketListener(websocket)
+    await deployments.add_listener(request.match_info['name'], listener)
+
+    try:
+        async for msg in websocket:
+            if msg.data == 'close':
+                await websocket.close()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        deployments.remove_listener(request.match_info['name'], listener)
+
+    await websocket.close()
+
+    return websocket
 
 async def page(request):
     name = request.match_info['name']
@@ -86,9 +154,10 @@ app.router.add_get('/page/{name}', page)
 app.router.add_get('/schema.yml', schema)
 app.router.add_static('/static/', path=os.path.join(os.path.dirname(__file__), 'static'), name='static')
 
-app.router.add_get('/deployments', deployments)
-app.router.add_post('/deployments/{name}', new_deployment)
-app.router.add_delete('/deployments/{name}', delete_deployment)
+app.router.add_get('/api/deployments', deployments.deployments_handler)
+app.router.add_post('/api/deployments/{name}', deployments.create_deployment_handler)
+app.router.add_delete('/api/deployments/{name}', deployments.destroy_deployment_handler)
+
 app.router.add_get('/stream/{name}', stream)
 
 web.run_app(app, port=5000)
